85920dbd36cec97e147aa49471ffaaa573316072
Andrew-Chen-Wang <acwangpython@gmail.com>
2021-06-22 01:33:33 -0400

    Finalize? * Test without username

--- index.js
+++ index.js
@@ -1,86 +1,78 @@
 const core = require('@actions/core');
 const fs = require('fs');
 const globby = require('globby');
+const simpleGit = require('simple-git');
+const path = require('path');
 
 async function getPath() {
-  // const filePath = core.getInput('path');
-  const filePath = "./districts.geojson"
-  try {
-    await fs.promises.access(filePath);
-    return filePath;
-  } catch (error) {
-    throw new Error(`File already exists at ${filePath}`);
-  }
+    const filePath = path.join(__dirname, core.getInput("path"));
+    return fs.promises.access(filePath, fs.constants.F_OK)
+        .then(() => {throw new Error(`File already exists at ${filePath}`)})
+        .catch(() => filePath);
 }
 
 async function compile(dir) {
-  // core.info("Compiling GeoJSON Objects into FeatureCollection");
-  let featureCollection = {
-    "type": "FeatureCollection",
-    "features": [],
-  }
-  let paths = await globby(`${dir}/cds/**/shape.geojson`);
-  // The format is typically in cds/year/STATE-DISTRICT-#/shape.geojson
-  // which means the year is ordered first after sort
-  paths.sort();
-  // format: state: {year: last year seen, data: []}
-  let statesSeen = {};
-  let fileLength = null;
-  paths.forEach((el) => {
-    fs.readFile(el, (err, data) => {
-      if (err) throw err;
-      el = el.split("/");
-      if (fileLength === null) fileLength = el.length;
-      const year = el[fileLength - 3];
-      const state = el[fileLength - 2].split("-")[0];
-      let val = statesSeen[state];
-      if (val === undefined || val.year !== year) {
-        statesSeen[state] = {year: year, data: [JSON.parse(data)]};
-      } else {
-        statesSeen[state].data.push(JSON.parse(data));
-      }
-    })
-  });
-  statesSeen.forEach(x => {featureCollection.features.concat(x.data);});
-  const compiledPath = await getPath()
-  fs.writeFile(compiledPath, JSON.stringify(featureCollection), (err) => {
-    throw new Error(`Couldn't write into the file. Error:\n${err}`);
-  });
-  // core.info(`Dumped file at ${compiledPath}`);
-  // core.setOutput("filePath", compiledPath);
-}
-
-async function run1() {
-  try {
-    // core.info("Downloading the repository: unitedstates/districts");
-    // const url = core.getInput("link");
-    const url = 'https://github.com/unitedstates/districts/archive/gh-pages.zip'
-    if (!url.endsWith(".zip")) {
-      core.setFailed("Link must be a zip file");
-      return;
+    core.info("Compiling GeoJSON Objects into FeatureCollection");
+    let featureCollection = {
+        "type": "FeatureCollection",
+        "features": [],
     }
-    // const dir = await download.getDistrictsRepo(url);
-    // await compile(dir);
-    // fs.unlink(dir, () => {});
-  } catch (error) {
-    // core.setFailed(error.message);
-  }
+    let paths = await globby(`${dir}/cds/**/shape.geojson`);
+    // The format is typically in cds/year/STATE-DISTRICT-#/shape.geojson
+    // which means the year is ordered first after sort
+    paths.sort();
+    // format: state: {year: last year seen, data: []}
+    let statesSeen = {};
+    let fileLength = null;
+    let i = 1;  // completion counter... never programming in JS with files again
+    for (let el of paths) {
+        await fs.readFile(el, "utf-8", (err, data) => {
+            if (err) {
+                i++;
+                throw err;
+            }
+            el = el.split("/");
+            if (fileLength === null) fileLength = el.length;
+            const year = el[fileLength - 3];
+            const state = el[fileLength - 2].split("-")[0];
+            let val = statesSeen[state];
+            if (val === undefined || val.year !== year) {
+                statesSeen[state] = {year: year, data: [JSON.parse(data)]};
+            } else {
+                statesSeen[state].data.push(JSON.parse(data));
+            }
+            i++;
+            core.info(`Processed: ${state} at ${year} (counter: ${i} / ${paths.length})`)
+        });
+    }
+    while (i !== paths.length) {
+        await new Promise(r => setTimeout(r, 2000));
+    }
+    for (const x of Object.values(statesSeen)) {
+        featureCollection.features.concat(x.data);
+    }
+    const compiledPath = await getPath();
+    await fs.writeFile(compiledPath, JSON.stringify(featureCollection), (err) => {
+        throw new Error(`Couldn't write into the file. Error:\n${err}`);
+    });
+    core.info(`Dumped file at ${compiledPath}`);
+    core.setOutput("filePath", compiledPath);
 }
 
-const simpleGit = require('simple-git');
-const path = require('path');
 async function run() {
-  const clonedDir = path.join(__dirname, `.district-${Math.random().toString(16)}`);
-  const git = simpleGit();
-  let repo = `https://Andrew-Chen-Wang:${core.getInput("token")}@github.com/${core.getInput("districts-repo")}.git`;
-  console.log("Cloning...");
-  await git.clone(repo, clonedDir);
-  await fs.readdir(clonedDir, (err, files) => {
-    files.forEach(file => {
-      console.log(file);
-    });
-  });
-  await git.cwd({path: clonedDir, root: true});
+    const clonedDir = path.join(__dirname, `.district-${Math.random().toString(16)}`);
+    const git = simpleGit();
+    let repo = `https://:${core.getInput("token")}@github.com/${core.getInput("districts-repo")}.git`;
+    core.info("Cloning...");
+    try {
+        await git.clone(repo, clonedDir);
+    } catch (e) {
+        core.error(e);
+        core.setFailed("Failed to clone repository");
+        return;
+    }
+    await git.cwd({path: clonedDir, root: true});
+    await compile(clonedDir);
 }
 
 run();
